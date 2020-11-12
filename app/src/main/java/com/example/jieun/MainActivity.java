463669f9c88a6f0fa9f361907c7172d8e18388bf
jieuntang <jieun127560@gmail.com>
2020-11-12 23:40:03 +0900

    사각형 절대좌표 이용해서 움직이기

--- app/src/main/java/com/example/jieun/MainActivity.java
+++ app/src/main/java/com/example/jieun/MainActivity.java
@@ -29,22 +29,84 @@ public class MainActivity extends AppCompatActivity {
         square = (Square)findViewById(R.id.square);
         final ImageView square = (ImageView) findViewById(R.id.imageView);
         Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
         Button button8 = (Button) findViewById(R.id.button8);
-        x = square.getX();
-        y = square.getY();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                x-=30;
-                y-=30;
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]-30);
+                square.setY(loc[1]-1200);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]);
+                square.setY(loc[1]-1200);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]+30);
+                square.setY(loc[1]-1200);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]-30);
+                square.setY(loc[1]-1050);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]+30);
+                square.setY(loc[1]-1050);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]-30);
+                square.setY(loc[1]-900);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]);
+                square.setY(loc[1]-900);
             }
         });
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                x+=30;
-                y+=30;
+                int[] loc = new int[2];
+                v.getLocationOnScreen(loc);
+                square.setX(loc[0]);
+                square.setY(loc[1]-900);
             }
         });
     }
